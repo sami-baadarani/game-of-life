e261ae617ee8d1541017ec44855aa45e55b80b6b
Sami Baadarani <samibaadarani@gmail.com>
2023-06-09 20:09:56 +1000

    ⚡️ feat: run generate next interval

--- src/components/GameOfLife/GameOfLife.vue
+++ src/components/GameOfLife/GameOfLife.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, type Ref } from 'vue';
 import LifeGrid from './LifeGrid/LifeGrid.vue';
 import { useGameOfLifeStore } from '@/stores/gameOfLife';
 import { watch } from 'vue';
 
 const dimension = ref(10)
+let generationInterval: Ref<NodeJS.Timeout | null> = ref(null)
 
 const gameOfLifeStore = useGameOfLifeStore()
 gameOfLifeStore.setDimension(dimension.value)
@@ -15,14 +16,32 @@ watch(dimension, (newDimension) => {
 
 function reset() {
     gameOfLifeStore.setDimension(dimension.value)
+    resetInterval()
 }
 
+function startGeneration() {
+    generationInterval.value = setInterval(() => {
+        gameOfLifeStore.nextGeneration()
+    }, 500);
+}
+
+function pauseGeneration() {
+    resetInterval()
+}
+
+function resetInterval() {
+    if (generationInterval.value) {
+        clearInterval(generationInterval.value)
+        generationInterval.value = null
+    }
+}
 </script>
 
 <template>
     <div>
         <input v-model="dimension" type="text" aria-label="Enter dimension">
-        <button>Start/Pause</button>
+        <button v-if="!generationInterval" @click="startGeneration">Start</button>
+        <button v-else @click="pauseGeneration">Pause</button>
         <button @click="reset">Reset</button>
         <LifeGrid :dimension="dimension" />
     </div>
