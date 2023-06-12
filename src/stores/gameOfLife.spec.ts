c64b64f35297b1b13fe6ea074decbcc346c631ae
Sami Baadarani <samibaadarani@gmail.com>
2023-06-12 21:34:39 +1000

    ✨ feat: allow each cell to have 8 siblings

--- src/stores/gameOfLife.spec.ts
+++ src/stores/gameOfLife.spec.ts
@@ -3,105 +3,105 @@ import { setActivePinia, createPinia } from 'pinia'
 import { useGameOfLifeStore } from '@/stores/gameOfLife'
 
 describe('gameOfLifeStore', () => {
-  beforeEach(() => {
-    setActivePinia(createPinia())
-  })
-  it('should have default dimension of 10', () => {
-    const gameOfLife = useGameOfLifeStore()
-    expect(gameOfLife.getDimension).toBe(10)
-  })
-  it('should allow dimension value to change', () => {
-    const gameOfLife = useGameOfLifeStore()
-    gameOfLife.setDimension(100)
-    expect(gameOfLife.getDimension).toBe(100)
-  })
-  it('should create an array using the dimension', () => {
-    const gameOfLife = useGameOfLifeStore()
-    expect(gameOfLife.cellList.length).toBe(100)
-  })
-  it('should update cellList when dimension is updated', () => {
-    const gameOfLife = useGameOfLifeStore()
-    gameOfLife.setDimension(100)
-    expect(gameOfLife.cellList.length).toBe(10000)
-  })
-  it('should toggle cell value at given index', () => {
-    const gameOfLife = useGameOfLifeStore()
-    gameOfLife.toggleCell(5)
-    expect(gameOfLife.cellList[5]).toBe(true)
-  })
+	beforeEach(() => {
+		setActivePinia(createPinia())
+	})
+	it('should have default dimension of 10', () => {
+		const gameOfLife = useGameOfLifeStore()
+		expect(gameOfLife.getDimension).toBe(10)
+	})
+	it('should allow dimension value to change', () => {
+		const gameOfLife = useGameOfLifeStore()
+		gameOfLife.setDimension(100)
+		expect(gameOfLife.getDimension).toBe(100)
+	})
+	it('should create an array using the dimension', () => {
+		const gameOfLife = useGameOfLifeStore()
+		expect(gameOfLife.cellList.length).toBe(100)
+	})
+	it('should update cellList when dimension is updated', () => {
+		const gameOfLife = useGameOfLifeStore()
+		gameOfLife.setDimension(100)
+		expect(gameOfLife.cellList.length).toBe(10000)
+	})
+	it('should toggle cell value at given index', () => {
+		const gameOfLife = useGameOfLifeStore()
+		gameOfLife.toggleCell(5)
+		expect(gameOfLife.cellList[5]).toBe(true)
+	})
 
-  describe('GameOfLife - Cell evaluation', () => {
-    it('should get all alive neighbors', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [false, true, false, true, false, true, false, true, false]
-      const neighbors = gameOfLife.getNeighbors(4)
-      expect(neighbors).toEqual([true, true, true, true])
-    })
-    it('should get all dead neighbors', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, false, true, false, true, false, true, false, true]
-      const neighbors = gameOfLife.getNeighbors(4)
-      expect(neighbors).toEqual([false, false, false, false])
-    })
-    it('should get all dead neighbors on extremities and alive neighbors elsewhere', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, true, true, true, true, false, true, false, true]
-      const neighbors = gameOfLife.getNeighbors(0)
-      expect(neighbors).toEqual([false, true, true, false])
-    })
-    it('should kill a cell with less than 2 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.evaluateCell(1)
-      expect(gameOfLife.cellList[1]).toBe(false)
-    })
-    it('should kill a cell with more than 3 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, true, true, true, true, true, true, true, true]
-      gameOfLife.evaluateCell(4)
-      expect(gameOfLife.cellList[4]).toBe(false)
-    })
-    it('should keep a cell alive with 2 or 3 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, true, true, false, true, false, true, true, true]
-      gameOfLife.evaluateCell(4)
-      expect(gameOfLife.cellList[4]).toBe(true)
-    })
-  })
+	describe('GameOfLife - Cell evaluation', () => {
+		it('should get neighbors alternating value starting with false', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [false, true, false, true, false, false, true, false, true]
+			const neighbors = gameOfLife.getNeighbors(4)
+			expect(neighbors).toEqual([false, true, false, true, false, true, false, true])
+		})
+		it('should get neighbors alternating value starting with true', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [true, false, true, false, true, true, false, true, false]
+			const neighbors = gameOfLife.getNeighbors(4)
+			expect(neighbors).toEqual([true, false, true, false, true, false, true, false])
+		})
+		it('should get all dead neighbors on extremities and alive neighbors elsewhere', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [true, true, true, true, true, true, true, true, true]
+			const neighbors = gameOfLife.getNeighbors(0)
+			expect(neighbors).toEqual([false, false, false, false, true, false, true, true])
+		})
+		it('should kill a cell with less than 2 neighbors alive', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.evaluateCell(1)
+			expect(gameOfLife.cellList[1]).toBe(false)
+		})
+		it('should kill a cell with more than 3 neighbors alive', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [true, true, true, true, true, true, true, true, true]
+			gameOfLife.evaluateCell(4)
+			expect(gameOfLife.cellList[4]).toBe(false)
+		})
+		it('should give life to cell alive with 2 or 3 neighbors alive', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [true, false, true, false, false, false, false, false, true]
+			gameOfLife.evaluateCell(4)
+			expect(gameOfLife.cellList[4]).toBe(true)
+		})
+	})
 
-  describe('GameOfLife - Rules', () => {
-    it('should kill a cell with less than 2 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [false, false, false, false, true, false, false, false, false]
-      gameOfLife.nextGeneration()
-      expect(gameOfLife.cellList[4]).toBe(false)
-    })
-    it('should kill a cell with more than 3 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, true, true, true, true, true, false, true, false]
-      gameOfLife.nextGeneration()
-      expect(gameOfLife.cellList[4]).toBe(false)
-    })
-    it('should keep a cell alive with 2 or 3 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, true, true, true, true, false, false, false, false]
-      gameOfLife.nextGeneration()
-      expect(gameOfLife.cellList[1]).toBe(true)
-    })
-    it('should revive a cell with exactly 3 neighbors alive', () => {
-      const gameOfLife = useGameOfLifeStore()
-      gameOfLife.setDimension(3)
-      gameOfLife.cellList = [true, true, true, true, false, true, false, false, false]
-      gameOfLife.nextGeneration()
-      expect(gameOfLife.cellList[4]).toBe(true)
-    })
-  })
+	describe('GameOfLife - Rules', () => {
+		it('should kill a cell with less than 2 neighbors alive', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [false, false, false, false, true, false, false, false, false]
+			gameOfLife.nextGeneration()
+			expect(gameOfLife.cellList[4]).toBe(false)
+		})
+		it('should kill a cell with more than 3 neighbors alive', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [true, true, true, true, true, true, false, true, false]
+			gameOfLife.nextGeneration()
+			expect(gameOfLife.cellList[4]).toBe(false)
+		})
+		it('should keep a cell alive with 2 or 3 neighbors alive', () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [true, true, true, true, false, false, false, false, false]
+			gameOfLife.nextGeneration()
+			expect(gameOfLife.cellList[1]).toBe(true)
+		})
+		it('should revive a cell with exactly 3 neighbors alive', async () => {
+			const gameOfLife = useGameOfLifeStore()
+			gameOfLife.setDimension(3)
+			gameOfLife.cellList = [false, false, true, true, false, true, false, false, false]
+			await gameOfLife.nextGeneration()
+			expect(gameOfLife.cellList[4]).toBe(true)
+		})
+	})
 })
--- src/stores/gameOfLife.ts
+++ src/stores/gameOfLife.ts
@@ -2,53 +2,63 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useGameOfLifeStore = defineStore('gameOfLife', () => {
-  const dimension = ref(10)
-  const cellList = ref(Array.from({ length: dimension.value * dimension.value }, () => false))
+	const dimension = ref(10)
+	const cellList = ref(Array.from({ length: dimension.value * dimension.value }, () => false))
 
-  const getDimension = computed(() => dimension.value)
+	const getDimension = computed(() => dimension.value)
 
-  function setDimension(value: number) {
-    dimension.value = value
-    cellList.value = Array.from({ length: dimension.value * dimension.value }, () => false)
-  }
+	function setDimension(value: number) {
+		dimension.value = value
+		cellList.value = Array.from({ length: dimension.value * dimension.value }, () => false)
+	}
 
-  function toggleCell(cellIndex: number) {
-    cellList.value[cellIndex] = !cellList.value[cellIndex]
-  }
+	function toggleCell(cellIndex: number) {
+		cellList.value[cellIndex] = !cellList.value[cellIndex]
+	}
 
-  function evaluateCell(cellIndex: number) {
-    const isAlive = cellList.value[cellIndex]
-    const neighbors = getNeighbors(cellIndex)
-    const aliveNeighbors = neighbors.filter((neighbor) => neighbor)
-    if (!isAlive) {
-      cellList.value[cellIndex] = aliveNeighbors.length === 3
-    } else {
-      cellList.value[cellIndex] = aliveNeighbors.length === 2 || aliveNeighbors.length === 3
-    }
-  }
+	function evaluateCell(cellIndex: number) {
+		const isAlive = cellList.value[cellIndex]
+		const neighbors = getNeighbors(cellIndex)
+		const aliveNeighbors = neighbors.filter((neighbor) => neighbor)
+		if (!isAlive) {
+			cellList.value[cellIndex] = aliveNeighbors.length === 3
+		} else {
+			cellList.value[cellIndex] = aliveNeighbors.length === 2 || aliveNeighbors.length === 3
+		}
+	}
 
-  function getNeighbors(cellIndex: number) {
-    const neighbors: boolean[] = []
-    neighbors.push(cellList.value[cellIndex - dimension.value] || false)
-    neighbors.push(cellList.value[cellIndex + 1] || false)
-    neighbors.push(cellList.value[cellIndex + dimension.value] || false)
-    neighbors.push(cellList.value[cellIndex - 1] || false)
-    return neighbors
-  }
+	function getNeighbors(cellIndex: number) {
+		const neighbors: boolean[] = []
+		neighbors.push(cellList.value[cellIndex - dimension.value - 1] || false)
+		neighbors.push(cellList.value[cellIndex - dimension.value] || false)
+		neighbors.push(cellList.value[cellIndex - dimension.value + 1] || false)
+		neighbors.push(cellList.value[cellIndex - 1] || false)
+		neighbors.push(cellList.value[cellIndex + 1] || false)
 
-  function nextGeneration() {
-    cellList.value.forEach((_, index) => {
-      evaluateCell(index)
-    })
-  }
+		neighbors.push(
+			(cellIndex + dimension.value) % dimension.value
+				? cellList.value[cellIndex + dimension.value - 1]
+				: false
+		)
 
-  return {
-    cellList,
-    evaluateCell,
-    getDimension,
-    getNeighbors,
-    nextGeneration,
-    setDimension,
-    toggleCell
-  }
+		neighbors.push(cellList.value[cellIndex + dimension.value] || false)
+		neighbors.push(cellList.value[cellIndex + dimension.value + 1] || false)
+		return neighbors
+	}
+
+	function nextGeneration() {
+		cellList.value.forEach((_, index) => {
+			evaluateCell(index)
+		})
+	}
+
+	return {
+		cellList,
+		evaluateCell,
+		getDimension,
+		getNeighbors,
+		nextGeneration,
+		setDimension,
+		toggleCell
+	}
 })
